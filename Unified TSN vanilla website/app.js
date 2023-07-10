5904dae4be5c13548a1afd32bbff55d27e23cc0f
Hashim Iqbal <mhi1@st-andrews.ac.uk>
2023-07-11 00:02:31 +0100

    Added automated sliding carousel for testimonials

--- Unified TSN vanilla website/app.js
+++ Unified TSN vanilla website/app.js
@@ -22,4 +22,53 @@ function toggleAnswer(index) {
         }
       }
     }
-}  
+}
+
+// Check if the device is mobile
+function isMobileDevice() {
+    return /Mobi/i.test(navigator.userAgent);
+}
+  
+function initSwipe() {
+    if (isMobileDevice()) {
+      var paragraphContainer = document.querySelector('.paragraph-container');
+      var hammertime = new Hammer(paragraphContainer);
+  
+      var currentIndex = 0;
+      var maxIndex = document.querySelectorAll('.paragraph-item').length - 1;
+  
+      hammertime.on('swipeleft', function() {
+        if (currentIndex < maxIndex) {
+          currentIndex++;
+          scrollToParagraph(currentIndex);
+        }
+      });
+  
+      hammertime.on('swiperight', function() {
+        if (currentIndex > 0) {
+          currentIndex--;
+          scrollToParagraph(currentIndex);
+        }
+      });
+    }
+}
+  
+window.addEventListener('DOMContentLoaded', function() {
+    initSwipe();
+});  
+  
+// Scroll to the selected paragraph
+function scrollToParagraph(index) {
+    var paragraphContainer = document.querySelector('.paragraph-container');
+    var paragraphItems = document.querySelectorAll('.paragraph-item');
+    
+    var scrollTo = paragraphItems[index].offsetLeft - paragraphContainer.offsetLeft;
+    
+    paragraphContainer.scrollTo({
+      left: scrollTo,
+      behavior: 'smooth'
+    });
+}
+  
+// Call the initSwipe function on page load
+window.addEventListener('load', initSwipe);  
