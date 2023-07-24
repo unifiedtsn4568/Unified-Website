a7bc29c26dd234ac124d711e5301152c344b9d1b
Hashim Iqbal <mhi1@st-andrews.ac.uk>
2023-07-24 18:16:42 +0100

    Added cards to home screen. Still need animation and images added

--- Unified TSN vanilla website/app.js
+++ Unified TSN vanilla website/app.js
@@ -71,4 +71,35 @@ function scrollToParagraph(index) {
 }
   
 // Call the initSwipe function on page load
-window.addEventListener('load', initSwipe);  
+window.addEventListener('load', initSwipe);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".carousel");
+  const prevArrow = document.querySelector(".prev-arrow");
+  const nextArrow = document.querySelector(".next-arrow");
+  const cards = document.querySelectorAll(".card");
+
+  let currentIndex = 0;
+
+  function showCard(index) {
+    cards.forEach((card, i) => {
+      card.style.display = i === index ? "block" : "none";
+    });
+  }
+
+  function showNextCard() {
+    currentIndex = (currentIndex + 1) % cards.length;
+    showCard(currentIndex);
+  }
+
+  function showPrevCard() {
+    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+    showCard(currentIndex);
+  }
+
+  nextArrow.addEventListener("click", showNextCard);
+  prevArrow.addEventListener("click", showPrevCard);
+
+  // Show the first card initially
+  showCard(currentIndex);
+});
